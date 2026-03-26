985bd4a49ab6bb0da89cbda5b5122a206a22289f
Mubs002 <mubeen123@live.com>
2026-03-26 17:18:39 +0000

    finished appointment logic

---
+++ backend/controllers/appointmentController.js
@@ -0,0 +1,33 @@
+const appointmentModel = require('../models/appointmentModel');
+
+const getAppointments = async (req, res) => {
+    try {
+        const appointments = await appointmentModel.getAllappointments();
+        res.json(appointments);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error fetching appointments');
+    }
+};
+
+const addAppointment = async (req, res) => {
+    try {
+        const { patient_id, professional_id, reason_for_visit } = req.body;
+
+        const appointment = await appointmentModel.createAppointment(
+            patient_id,
+            professional_id,
+            reason_for_visit
+        );
+
+        res.json(appointment);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error creating appointment');
+    }
+};
+
+module.exports = {
+    getAppointments,
+    addAppointment
+};
