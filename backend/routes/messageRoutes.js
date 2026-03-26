edf9b1f1406b9b0bd751fff0c14231f61efe5785
Mubs002 <mubeen123@live.com>
2026-03-26 18:07:47 +0000

    putting message routes here

---
+++ backend/routes/messageRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const messageController = require('../controllers/messageController');
+
+router.get('/threads', messageController.getThreads);
+router.post('/threads', messageController.addThreads);
+router.get('/threads/:threadId/messages', messageController.getMessages);
+router.post('/threads/:threadId/messages', messageController.addMessage);
+router.put('/messages/:messageId/read', messageController.updateMessageRead);
+
+module.exports = router;
