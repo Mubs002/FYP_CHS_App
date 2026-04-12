aebccabd7d129f1753c182de120b6dd040533380
Mubs002 <mubeen123@live.com>
2026-04-12 19:24:54 +0100

    added appointments page placeholder

---
+++ frontend/src/view/pages/AppointmentsPage.jsx
@@ -0,0 +1,4 @@
+// placeholder - will be built in the next step
+export default function AppointmentsPage() {
+  return <div style={{ padding: '40px', textAlign: 'center' }}>Appointments - Coming Soon</div>;
+}
