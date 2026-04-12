3665a6e6f483cbad4011a6272c2972ca845ec4fc
Mubs002 <mubeen123@live.com>
2026-04-12 19:24:29 +0100

    added register page placeholder

---
+++ frontend/src/view/pages/RegisterPage.jsx
@@ -0,0 +1,4 @@
+// placeholder - will be built in the next step
+export default function RegisterPage() {
+  return <div style={{ padding: '40px', textAlign: 'center' }}>Register Page - Coming Soon</div>;
+}
