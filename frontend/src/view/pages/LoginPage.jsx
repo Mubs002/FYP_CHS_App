071009e219d86feb78c87eb85b8d8c243b8ae430
Mubs002 <mubeen123@live.com>
2026-04-12 19:24:20 +0100

    added login page placeholder

---
+++ frontend/src/view/pages/LoginPage.jsx
@@ -0,0 +1,4 @@
+// placeholder - will be built in the next step
+export default function LoginPage() {
+  return <div style={{ padding: '40px', textAlign: 'center' }}>Login Page - Coming Soon</div>;
+}
