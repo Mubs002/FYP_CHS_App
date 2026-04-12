9c3f1d5b3ce2f17336d2735f923d2ae0ad29fe31
Mubs002 <mubeen123@live.com>
2026-04-12 19:25:17 +0100

    added mental health page placeholder

---
+++ frontend/src/view/pages/MentalHealthPage.jsx
@@ -0,0 +1,4 @@
+// placeholder - will be built in the next step
+export default function MentalHealthPage() {
+  return <div style={{ padding: '40px', textAlign: 'center' }}>Mental Health - Coming Soon</div>;
+}
