2e5348bc877f26aecf0c4d6c88ed03d41d38d715
Mubs002 <mubeen123@live.com>
2026-04-13 15:56:01 +0100

    added usestate for firstname lastname email password role error and loading

--- frontend/src/view/pages/RegisterPage.jsx
+++ frontend/src/view/pages/RegisterPage.jsx
@@ -9,4 +9,20 @@ export default function RegisterPage() {
   // reading the role that was passed from the landing page role selection
   const location = useLocation();
   const passedRole = location.state?.role || 'patient';
+
+  //form field in its own state variable
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  // role defaults to whatever user picked landing pagee
+  const [role, setRole] = useState(passedRole);
+
+  // shows an error message if registration fails
+  const [error, setError] = useState('');
+
+  // disables button while the request is in progress
+  const [loading, setLoading] = useState(false);
+
 }
