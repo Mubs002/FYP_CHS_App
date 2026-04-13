f1d5078ec49ad4637d79369a26e74d1308e3583d
Mubs002 <mubeen123@live.com>
2026-04-13 15:12:19 +0100

    added basic imports to layout loginpage

--- frontend/src/view/pages/LoginPage.jsx
+++ frontend/src/view/pages/LoginPage.jsx
@@ -1,4 +1,8 @@
-// placeholder - will be built in the next step
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { loginUser } from '../../model/api/api';
+import { useAuth } from '../../model/context/AuthContext';
+import './LoginPage.css';
 export default function LoginPage() {
   return <div style={{ padding: '40px', textAlign: 'center' }}>Login Page - Coming Soon</div>;
 }
