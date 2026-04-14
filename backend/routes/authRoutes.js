011a94cab7c3055314370286fa90b1cdd17b26f6
Mubs002 <mubeen123@live.com>
2026-04-14 14:39:53 +0100

    add settings routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -5,4 +5,9 @@ const authController = require('../controllers/authController');
 router.post('/register', authController.registerUser);
 router.post('/login', authController.loginUser);
 
+// i added these routes for the settings page
+router.get('/user/:id', authController.getUser);
+router.put('/user/:id/profile', authController.updateProfile);
+router.put('/user/:id/password', authController.updatePassword);
+
 module.exports = router;
