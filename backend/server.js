84897f63d216d674c038b004e9edf76f060004c7
Mubs002 <mubeen123@live.com>
2026-04-14 03:15:28 +0100

    register health record routes in server

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const PORT = 3000;
 const appointmentRoutes = require('./routes/appointmentRoutes');
 const messageRoutes = require('./routes/messageRoutes');
 const authRoutes = require('./routes/authRoutes');
+const healthRecordRoutes = require('./routes/healthRecordRoutes');
 
 // allows requets from the react frontend on 5173 port
 app.use(cors({ origin: 'http://localhost:5173' }));
@@ -18,6 +19,10 @@ app.get('/', (req, res) => {
 app.use('/appointments', appointmentRoutes);
 app.use('/', messageRoutes);
 app.use('/', authRoutes);
+app.use('/health-records', healthRecordRoutes);
+
+// i added this so the frontend can access uploaded files directly by url
+app.use('/uploads', express.static('uploads'));
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
